adcd40f7180f7ec00b1b00862daa3fef085f4a44
PanshulParth <panshulparth123@gmail.com>
2026-04-02 16:04:27 +0530

    UC6: Floor and Ceiling Binary Search

--- src/Assignment_Week_3_4.java
+++ src/Assignment_Week_3_4.java
@@ -1,31 +1,22 @@
 
 public class Assignment_Week_3_4 {
 
-    static int linearSearch(String[] arr, String target) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].equals(target)) return i;
-        }
-        return -1;
+    static int floor(int[] arr, int target) {
+        int res = -1;
+        for (int x : arr) if (x <= target) res = x;
+        return res;
     }
 
-    static int binarySearch(String[] arr, String target) {
-        int l = 0, r = arr.length - 1;
-
-        while (l <= r) {
-            int mid = (l + r) / 2;
-
-            if (arr[mid].equals(target)) return mid;
-            if (arr[mid].compareTo(target) < 0) l = mid + 1;
-            else r = mid - 1;
-        }
+    static int ceiling(int[] arr, int target) {
+        for (int x : arr) if (x >= target) return x;
         return -1;
     }
 
     public static void main(String[] args) {
-        String[] arr = {"accA", "accB", "accC"};
+        int[] arr = {10, 25, 50, 100};
 
-        System.out.println("Linear: " + linearSearch(arr, "accB"));
-        System.out.println("Binary: " + binarySearch(arr, "accB"));
+        System.out.println("Floor: " + floor(arr, 30));
+        System.out.println("Ceiling: " + ceiling(arr, 30));
     }
 }
 
